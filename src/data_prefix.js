0a96bafcc28fd298736b937324a1ba6e3a30a362
Tatiroa994 <tatico994@gmail.com>
2021-04-22 16:00:42 -0500

    style

---
+++ src/data_prefix.js
@@ -0,0 +1,1103 @@
+export const countries = [
+  {
+    country: "Albania",
+    prefix: "355",
+  },
+  {
+    country: "Alemania",
+    prefix: "49",
+  },
+  {
+    country: "Angola",
+    prefix: "244",
+  },
+  {
+    country: "Anguila",
+    prefix: "1",
+  },
+  {
+    country: "Antigua y Barbuda",
+    prefix: "1",
+  },
+  {
+    country: "Antillas Holandesas",
+    prefix: "599",
+  },
+  {
+    country: "Arabia Saudita",
+    prefix: "966",
+  },
+  {
+    country: "Argelia",
+    prefix: "213",
+  },
+  {
+    country: "Argentina",
+    prefix: "54",
+  },
+  {
+    country: "Armenia",
+    prefix: "374",
+  },
+  {
+    country: "Aruba",
+    prefix: "297",
+  },
+  {
+    country: "Australia",
+    prefix: "61",
+  },
+  {
+    country: "Austria",
+    prefix: "43",
+  },
+  {
+    country: "Azerbaiyán",
+    prefix: "994",
+  },
+  {
+    country: "Bahamas",
+    prefix: "1",
+  },
+  {
+    country: "Bangladés",
+    prefix: "880",
+  },
+  {
+    country: "Barbados",
+    prefix: "1",
+  },
+  {
+    country: "Bélgica",
+    prefix: "32",
+  },
+  {
+    country: "Belice",
+    prefix: "501",
+  },
+  {
+    country: "Benín",
+    prefix: "229",
+  },
+  {
+    country: "Bermudas",
+    prefix: "1",
+  },
+  {
+    country: "Bielorrusia",
+    prefix: "375",
+  },
+  {
+    country: "Bolivia",
+    prefix: "591",
+  },
+  {
+    country: "Bosnia y Herzegovina",
+    prefix: "387",
+  },
+  {
+    country: "Botsuana",
+    prefix: "267",
+  },
+  {
+    country: "Brasil",
+    prefix: "55",
+  },
+  {
+    country: "0015 = Vivo",
+  },
+  {
+    country: "0021 = Claro",
+  },
+  {
+    country: "0025 = GVT",
+  },
+  {
+    country: "0031 = Oi",
+  },
+  {
+    country: "0041 = TIM",
+  },
+  {
+    country: "0012 = CTBC",
+    prefix: "0",
+  },
+  {
+    country: "Brunéi",
+    prefix: "673",
+  },
+  {
+    country: "Bulgaria",
+    prefix: "359",
+  },
+  {
+    country: "Burkina Faso",
+    prefix: "226",
+  },
+  {
+    country: "Burundi",
+    prefix: "257",
+  },
+  {
+    country: "Bután",
+    prefix: "975",
+  },
+  {
+    country: "Cabo Verde",
+    prefix: "238",
+  },
+  {
+    country: "Camboya",
+    prefix: "855",
+  },
+  {
+    country: "7",
+  },
+  {
+    country: "8",
+    prefix: "0",
+  },
+  {
+    country: "Camerún",
+    prefix: "237",
+  },
+  {
+    country: "Canadá",
+    prefix: "1",
+  },
+  {
+    country: "Catar",
+    prefix: "974",
+  },
+  {
+    country: "Chad",
+    prefix: "235",
+  },
+  {
+    country: "Chequia",
+    prefix: "420",
+  },
+  {
+    country: "Chile",
+    prefix: "56",
+  },
+  {
+    country: "1220 = Manquehue",
+  },
+  {
+    country: "1230 = Entel",
+  },
+  {
+    country: "1690 = Netline",
+  },
+  {
+    country: "1710 = Claro",
+  },
+  {
+    country: "1810 = Movistar",
+    prefix: "nada",
+  },
+  {
+    country: "China",
+    prefix: "86",
+  },
+  {
+    country: "Chipre",
+    prefix: "357",
+  },
+  {
+    country: "Colombia",
+    prefix: "57",
+  },
+  {
+    country: "005 = UNE EPM",
+  },
+  {
+    country: "007 = ETB",
+  },
+  {
+    country: "009 = Movistar",
+  },
+  {
+    country: "00414 = Tigo",
+  },
+  {
+    country: "00444 = Claro Móvil",
+  },
+  {
+    country: "00456 = Claro fijos",
+  },
+  {
+    country: "00468 = Avantel",
+    prefix: "03 = Móviles",
+  },
+  {
+    country: "05 = UNE EPM",
+  },
+  {
+    country: "07 = ETB",
+  },
+  {
+    country: "09 = Movistar",
+  },
+  {
+    country: "0414 = Tigo",
+  },
+  {
+    country: "0444 = Claro Móvil",
+  },
+  {
+    country: "0456 = Claro fijos",
+  },
+  {
+    country: "0468 = Avantel",
+  },
+  {
+    country: "Comoras",
+    prefix: "269",
+  },
+  {
+    country: "Congo",
+    prefix: "242",
+  },
+  {
+    country: "Corea",
+    prefix: "82",
+  },
+  {
+    country: "2",
+    prefix: "0",
+  },
+  {
+    country: "Corea del Norte",
+    prefix: "850",
+  },
+  {
+    country: "99",
+    prefix: "0",
+  },
+  {
+    country: "Costa de Marfil",
+    prefix: "225",
+  },
+  {
+    country: "Costa Rica",
+    prefix: "506",
+  },
+  {
+    country: "Croacia",
+    prefix: "385",
+  },
+  {
+    country: "Cuba",
+    prefix: "53",
+  },
+  {
+    country: "Diego García",
+    prefix: "246",
+  },
+  {
+    country: "Dinamarca",
+    prefix: "45",
+  },
+  {
+    country: "Dominica",
+    prefix: "1",
+  },
+  {
+    country: "Ecuador",
+    prefix: "593",
+  },
+  {
+    country: "Egipto",
+    prefix: "20",
+  },
+  {
+    country: "El Salvador",
+    prefix: "503",
+  },
+  {
+    country: "Emiratos Árabes Unidos",
+    prefix: "971",
+  },
+  {
+    country: "Eritrea",
+    prefix: "291",
+  },
+  {
+    country: "Escocia",
+    prefix: "44",
+  },
+  {
+    country: "Eslovaquia",
+    prefix: "421",
+  },
+  {
+    country: "Eslovenia",
+    prefix: "386",
+  },
+  {
+    country: "España",
+    prefix: "34",
+  },
+  {
+    country: "Estonia",
+    prefix: "372",
+  },
+  {
+    country: "Etiopía",
+    prefix: "251",
+  },
+  {
+    country: "Federación Rusa",
+    prefix: "7",
+  },
+  {
+    country: "Filipinas",
+    prefix: "63",
+  },
+  {
+    country: "Finlandia",
+    prefix: "358",
+  },
+  {
+    country: "990",
+  },
+  {
+    country: "994",
+  },
+  {
+    country: "999",
+    prefix: "0",
+  },
+  {
+    country: "Fiyi",
+    prefix: "679",
+  },
+  {
+    country: "Francia",
+    prefix: "33",
+  },
+  {
+    country: "Gabón",
+    prefix: "241",
+  },
+  {
+    country: "Gales",
+    prefix: "44",
+  },
+  {
+    country: "Gambia",
+    prefix: "220",
+  },
+  {
+    country: "Georgia",
+    prefix: "995",
+  },
+  {
+    country: "Ghana",
+    prefix: "233",
+  },
+  {
+    country: "Gibraltar",
+    prefix: "350",
+  },
+  {
+    country: "Granada",
+    prefix: "1",
+  },
+  {
+    country: "Grecia",
+    prefix: "30",
+  },
+  {
+    country: "Groenlandia",
+    prefix: "299",
+  },
+  {
+    country: "Guadalupe",
+    prefix: "590",
+  },
+  {
+    country: "Guam",
+    prefix: "1",
+  },
+  {
+    country: "Guatemala",
+    prefix: "502",
+  },
+  {
+    country: "Guayana Francesa",
+    prefix: "594",
+  },
+  {
+    country: "Guinea",
+    prefix: "224",
+  },
+  {
+    country: "Guinea Ecuatorial",
+    prefix: "240",
+  },
+  {
+    country: "Guinea-Bissau",
+    prefix: "245",
+  },
+  {
+    country: "Guyana",
+    prefix: "592",
+  },
+  {
+    country: "Haití",
+    prefix: "509",
+  },
+  {
+    country: "Holanda",
+    prefix: "31",
+  },
+  {
+    country: "Honduras",
+    prefix: "504",
+  },
+  {
+    country: "HongKong",
+    prefix: "852",
+  },
+  {
+    country: "Hungría",
+    prefix: "36",
+  },
+  {
+    country: "India",
+    prefix: "91",
+  },
+  {
+    country: "Indonesia",
+    prefix: "62",
+  },
+  {
+    country: "007 = Telkom",
+  },
+  {
+    country: "008 = Indosat",
+  },
+  {
+    country: "009 = Bakrie Telecom",
+    prefix: "0",
+  },
+  {
+    country: "Inglaterra",
+    prefix: "44",
+  },
+  {
+    country: "Irak",
+    prefix: "964",
+  },
+  {
+    country: "Irán",
+    prefix: "98",
+  },
+  {
+    country: "Irlanda",
+    prefix: "353",
+  },
+  {
+    country: "Irlanda del Norte",
+    prefix: "44",
+  },
+  {
+    country: "Isla Ascensión",
+    prefix: "247",
+  },
+  {
+    country: "Isla Norfolk",
+    prefix: "6723",
+  },
+  {
+    country: "Islandia",
+    prefix: "354",
+  },
+  {
+    country: "Islas Caimán",
+    prefix: "1",
+  },
+  {
+    country: "Islas Cook",
+    prefix: "682",
+  },
+  {
+    country: "Islas Feroe",
+    prefix: "298",
+  },
+  {
+    country: "Islas Malvinas",
+    prefix: "500",
+  },
+  {
+    country: "Islas Marianas del Norte",
+    prefix: "1",
+  },
+  {
+    country: "Islas Marshall",
+    prefix: "692",
+  },
+  {
+    country: "Islas Salomón",
+    prefix: "677",
+  },
+  {
+    country: "Islas Turcas y Caicos",
+    prefix: "1",
+  },
+  {
+    country: "Islas Vírgenes Británicas",
+    prefix: "1",
+  },
+  {
+    country: "Islas Vírgenes de los Estados Unidos",
+    prefix: "1",
+  },
+  {
+    country: "Israel",
+    prefix: "972",
+  },
+  {
+    country: "12",
+  },
+  {
+    country: "13",
+  },
+  {
+    country: "14",
+  },
+  {
+    country: "18",
+    prefix: "0",
+  },
+  {
+    country: "Italia",
+    prefix: "39",
+  },
+  {
+    country: "Jamaica",
+    prefix: "1",
+  },
+  {
+    country: "Japón",
+    prefix: "81",
+  },
+  {
+    country: "Jordania",
+    prefix: "962",
+  },
+  {
+    country: "Kazajistán",
+    prefix: "7",
+  },
+  {
+    country: "Kenia",
+    prefix: "254",
+  },
+  {
+    country: "Kirguistán",
+    prefix: "996",
+  },
+  {
+    country: "Kiribati",
+    prefix: "686",
+  },
+  {
+    country: "Kuwait",
+    prefix: "965",
+  },
+  {
+    country: "Laos",
+    prefix: "856",
+  },
+  {
+    country: "Lesoto",
+    prefix: "266",
+  },
+  {
+    country: "Letonia",
+    prefix: "371",
+  },
+  {
+    country: "Líbano",
+    prefix: "961",
+  },
+  {
+    country: "Liberia",
+    prefix: "231",
+  },
+  {
+    country: "Libia",
+    prefix: "218",
+  },
+  {
+    country: "Liechtenstein",
+    prefix: "423",
+  },
+  {
+    country: "Lituania",
+    prefix: "370",
+  },
+  {
+    country: "Luxemburgo",
+    prefix: "352",
+  },
+  {
+    country: "Macao",
+    prefix: "853",
+  },
+  {
+    country: "Macedonia",
+    prefix: "389",
+  },
+  {
+    country: "Madagascar",
+    prefix: "261",
+  },
+  {
+    country: "Malasia",
+    prefix: "60",
+  },
+  {
+    country: "Malaui",
+    prefix: "265",
+  },
+  {
+    country: "Maldivas",
+    prefix: "960",
+  },
+  {
+    country: "Malí",
+    prefix: "223",
+  },
+  {
+    country: "Malta",
+    prefix: "356",
+  },
+  {
+    country: "Marruecos",
+    prefix: "212",
+  },
+  {
+    country: "Martinica",
+    prefix: "596",
+  },
+  {
+    country: "Mauricio",
+    prefix: "230",
+  },
+  {
+    country: "Mauritania",
+    prefix: "222",
+  },
+  {
+    country: "Mayotte",
+    prefix: "262",
+  },
+  {
+    country: "México",
+    prefix: "52",
+  },
+  {
+    country: "Micronesia",
+    prefix: "691",
+  },
+  {
+    country: "Moldavia",
+    prefix: "373",
+  },
+  {
+    country: "Mónaco",
+    prefix: "377",
+  },
+  {
+    country: "Mongolia",
+    prefix: "976",
+  },
+  {
+    country: "2",
+  },
+  {
+    country: "Montenegro",
+    prefix: "382",
+  },
+  {
+    country: "Montserrat",
+    prefix: "1",
+  },
+  {
+    country: "Mozambique",
+    prefix: "258",
+  },
+  {
+    country: "Myanmar",
+    prefix: "95",
+  },
+  {
+    country: "Namibia",
+    prefix: "264",
+  },
+  {
+    country: "Nauru",
+    prefix: "674",
+  },
+  {
+    country: "Nepal",
+    prefix: "977",
+  },
+  {
+    country: "Nicaragua",
+    prefix: "505",
+  },
+  {
+    country: "Níger",
+    prefix: "227",
+  },
+  {
+    country: "Nigeria",
+    prefix: "234",
+  },
+  {
+    country: "Niue",
+    prefix: "683",
+  },
+  {
+    country: "Noruega",
+    prefix: "47",
+  },
+  {
+    country: "Nueva Caledonia",
+    prefix: "687",
+  },
+  {
+    country: "Nueva Zelanda",
+    prefix: "64",
+  },
+  {
+    country: "Omán",
+    prefix: "968",
+  },
+  {
+    country: "Pakistán",
+    prefix: "92",
+  },
+  {
+    country: "Palaos",
+    prefix: "680",
+  },
+  {
+    country: "12",
+    prefix: "nada",
+  },
+  {
+    country: "Palestina",
+    prefix: "970",
+  },
+  {
+    country: "Panamá",
+    prefix: "507",
+  },
+  {
+    country: "Papúa Nueva Guinea",
+    prefix: "675",
+  },
+  {
+    country: "Paraguay",
+    prefix: "595",
+  },
+  {
+    country: "Perú",
+    prefix: "51",
+  },
+  {
+    country: "Polinesia Francesa",
+    prefix: "689",
+  },
+  {
+    country: "Polonia",
+    prefix: "48",
+  },
+  {
+    country: "Portugal",
+    prefix: "351",
+  },
+  {
+    country: "Principado de Andorra",
+    prefix: "376",
+  },
+  {
+    country: "Puerto Rico",
+    prefix: "1",
+  },
+  {
+    country: "Reino de Bahréin",
+    prefix: "973",
+  },
+  {
+    country: "Rep. Dominicana",
+    prefix: "1",
+  },
+  {
+    country: "República Centroafricana",
+    prefix: "236",
+  },
+  {
+    country: "República Democrática del Congo",
+    prefix: "243",
+  },
+  {
+    country: "Reunión",
+    prefix: "262",
+  },
+  {
+    country: "Ruanda",
+    prefix: "250",
+  },
+  {
+    country: "Rumanía",
+    prefix: "40",
+  },
+  {
+    country: "Sáhara Occidental",
+    prefix: "212",
+  },
+  {
+    country: "Samoa",
+    prefix: "685",
+  },
+  {
+    country: "Samoa Americana",
+    prefix: "1",
+  },
+  {
+    country: "San Bartolomé",
+    prefix: "590",
+  },
+  {
+    country: "San Cristóbal y Nieves",
+    prefix: "1",
+  },
+  {
+    country: "San Marino",
+    prefix: "378",
+  },
+  {
+    country: "San Martín",
+    prefix: "590",
+  },
+  {
+    country: "San Pedro y Miquelón",
+    prefix: "508",
+  },
+  {
+    country: "San Vicente y las Granadinas",
+    prefix: "1",
+  },
+  {
+    country: "Santa Elena",
+    prefix: "290",
+  },
+  {
+    country: "Santa Lucía",
+    prefix: "1",
+  },
+  {
+    country: "Santo Tomé y Príncipe",
+    prefix: "239",
+  },
+  {
+    country: "Satélite Inmarsat",
+    prefix: "870",
+  },
+  {
+    country: "Satélite Iridium",
+    prefix: "8816",
+  },
+  {
+    country: "8817",
+    prefix: "0",
+  },
+  {
+    country: "Satélite Thuraya",
+    prefix: "882 16",
+  },
+  {
+    country: "Senegal",
+    prefix: "221",
+  },
+  {
+    country: "Serbia",
+    prefix: "381",
+  },
+  {
+    country: "Seychelles",
+    prefix: "248",
+  },
+  {
+    country: "Sierra Leona",
+    prefix: "232",
+  },
+  {
+    country: "Singapur",
+    prefix: "65",
+  },
+  {
+    country: "8",
+    prefix: "nada",
+  },
+  {
+    country: "Sint Maarten",
+    prefix: "1",
+  },
+  {
+    country: "Siria",
+    prefix: "963",
+  },
+  {
+    country: "Somalia",
+    prefix: "252",
+  },
+  {
+    country: "Sri Lanka",
+    prefix: "94",
+  },
+  {
+    country: "Suazilandia",
+    prefix: "268",
+  },
+  {
+    country: "Sudáfrica",
+    prefix: "27",
+  },
+  {
+    country: "Sudán",
+    prefix: "249",
+  },
+  {
+    country: "Sudán del Sur",
+    prefix: "211",
+  },
+  {
+    country: "Suecia",
+    prefix: "46",
+  },
+  {
+    country: "Suiza",
+    prefix: "41",
+  },
+  {
+    country: "Surinam",
+    prefix: "597",
+  },
+  {
+    country: "Tailandia",
+    prefix: "66",
+  },
+  {
+    country: "Taiwán",
+    prefix: "886",
+  },
+  {
+    country: "Tanzania",
+    prefix: "255",
+  },
+  {
+    country: "Tayikistán",
+    prefix: "992",
+  },
+  {
+    country: "Timor Oriental",
+    prefix: "670",
+  },
+  {
+    country: "Togo",
+    prefix: "228",
+  },
+  {
+    country: "Tokelau",
+    prefix: "690",
+  },
+  {
+    country: "Tonga",
+    prefix: "676",
+  },
+  {
+    country: "Trinidad y Tobago",
+    prefix: "1",
+  },
+  {
+    country: "Túnez",
+    prefix: "216",
+  },
+  {
+    country: "Turkmenistán",
+    prefix: "993",
+  },
+  {
+    country: "Turquía",
+    prefix: "90",
+  },
+  {
+    country: "Tuvalu",
+    prefix: "688",
+  },
+  {
+    country: "Ucrania",
+    prefix: "380",
+  },
+  {
+    country: "Uganda",
+    prefix: "256",
+  },
+  {
+    country: "Uruguay",
+    prefix: "598",
+  },
+  {
+    country: "USA",
+    prefix: "1",
+  },
+  {
+    country: "Uzbekistán",
+    prefix: "998",
+  },
+  {
+    country: "Vanuatu",
+    prefix: "678",
+  },
+  {
+    country: "Vaticano",
+    prefix: "39",
+  },
+  {
+    country: "Venezuela",
+    prefix: "58",
+  },
+  {
+    country: "Vietnam",
+    prefix: "84",
+  },
+  {
+    country: "Wallis y Futuna",
+    prefix: "681",
+  },
+  {
+    country: "Yemen",
+    prefix: "967",
+  },
+  {
+    country: "Yibuti",
+    prefix: "253",
+  },
+  {
+    country: "Zambia",
+    prefix: "260",
+  },
+  {
+    country: "Zimbabue",
+    prefix: "263",
+  },
+];
